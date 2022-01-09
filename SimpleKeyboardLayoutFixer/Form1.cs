05a81b1c89793faa359fa8b55a25b8ec89029d97
Cranch <50642181+Cranch-fur@users.noreply.github.com>
2022-01-09 06:20:49 +0300

    [1.0.0.0]

---
+++ SimpleKeyboardLayoutFixer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management.Automation;
+/// <summary>
+///  NuGet package used -> https://www.nuget.org/packages/Cake.Powershell
+///  Languages codes list -> http://www.lingoes.net/en/translator/langcode.htm
+/// </summary>
+
+namespace SimpleKeyboardLayoutFixer
+{
+    public partial class Form1 : Form
+    {
+        public PowerShell powerShell;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            powerShell = PowerShell.Create();
+            while (true)
+            {
+                string userSpecifiedLanguageTwoLetterCode = InputLanguage.CurrentInputLanguage.Culture.TwoLetterISOLanguageName;
+                switch (userSpecifiedLanguageTwoLetterCode)
+                {
+                    case "ru":
+                        powerShell.AddScript("Set-WinUserLanguageList -LanguageList ru, en-US -Force").Invoke();
+                        break;
+                    case "en":
+                        powerShell.AddScript("Set-WinUserLanguageList -LanguageList en-US, ru -Force").Invoke();
+                        break;
+                }
+                System.Threading.Thread.Sleep(900000);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) => powerShell.Dispose();
+    }
+}
